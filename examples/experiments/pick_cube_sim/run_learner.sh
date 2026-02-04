53b7cd727f72b48bd1d21f394ebcdfd0e66e516c
yesen-chen <840419490@qq.com>
2026-02-05 01:05:39 +0800

    [fix] fix bug of jax version

--- examples/experiments/pick_cube_sim/run_learner.sh
+++ examples/experiments/pick_cube_sim/run_learner.sh
@@ -3,5 +3,5 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python ../../train_rlpd.py "$@" \
     --exp_name=pick_cube_sim \
     --checkpoint_path=first_run \
-    --demo_path=/home/jyang159/projects/hil-serl/examples/experiments/pick_cube_sim/demo_data/pick_cube_sim_20_demos_2025-01-22_18-19-38.pkl \
+    --demo_path=/home/zhang/robot/hil-serl-sim/examples/experiments/pick_cube_sim/demo_data/pick_cube_sim_20_demos_2026-02-04_21-14-10.pkl \
     --learner \
